d268657790f3397f9374ce356b1a0efadc5dc95d
Christan Kalu <christiankalu020@gmail.com>
2026-02-09 23:24:25 +0100

    Continued on the Profile Page

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -2,12 +2,13 @@ import TopBar from "../Components/UI/TopBar";
 import Christian from "../assets/Christian.jpg"
 
 function UserProfile () {
+    const name = "Christian Kalu";
 
     return (
         <div className="p-6 max-w-6xl mx-auto">
             <TopBar title="My Profile" />
 
-            <div className="flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center gap-6">
                 <div className="flex items-center -space-x-2">
                     <img
                     src={Christian}
@@ -16,10 +17,14 @@ function UserProfile () {
                     />
                 </div>
 
-                {/* Deadline Text (Red/Rose color) */}
-                <span className="text-sm font-medium text-rose-400">
+                <div className="flex flex-col justify-center items-center gap">
+                    <h3 className="text-2xl font-bold text-white">{name}</h3>
+                    <span className="text-slate-400">Web Developer</span>
+                </div>
 
-                </span>
+                <button className="bg-cyan-500 hover:bg-cyan-600 text-black font-bold py-2 px-4 rounded transition-colors">
+                    Edit Profile
+                </button>
             </div>
         </div>
     )
